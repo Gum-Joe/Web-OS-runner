8ce00a95cad8f7d7ab71d9a07089eef901135918
Gum-Joe <kishansambhi@hotmail.co.uk>
2015-10-17 15:45:18 +0100

    O

--- nodejs/run.js
+++ nodejs/run.js
@@ -1,13 +1,20 @@
 // Runs web-app
 var express = require('express');
-var spawn = require('child_process').spwan
+var spawn = require('child_process')
 var app = express();
 
 var port = process.env.TUB_RECIEVER_PORT
-app.listen(3001, function () {
+app.listen(port, function () {
   console.log('Listening...');
 });
 
 app.get('/', function (req, res) {
   res.send('Starting Web-app...')
+  var w = spawn.spawn('sh', ['./start.sh'])
+  w.stdout.on('data', function (data) {
+    res.send(data)
+  });
+  w.stderr.on('data', function (data) {
+    res.send(data)
+  });
 });

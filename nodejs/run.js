ba0f854af5261ce34c587810ecde19020c52adc8
Gum-Joe <kishansambhi@hotmail.co.uk>
2015-10-17 15:38:20 +0100

    Add server listen

--- nodejs/run.js
+++ nodejs/run.js
@@ -1,1 +1,13 @@
 // Runs web-app
+var express = require('express');
+var spawn = require('child_process').spwan
+var app = express();
+
+var port = process.env.TUB_RECIEVER_PORT
+app.listen(3001, function () {
+  console.log('Listening...');
+});
+
+app.get('/', function (req, res) {
+  res.send('Starting Web-app...')
+});
